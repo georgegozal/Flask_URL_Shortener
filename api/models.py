81a19c07511260fb6459f3df9c6ce5d649dc12cd
georgegozal <georgeg9@protonmail.com>
2022-07-14 12:01:29 +0400

    changed date filed

--- api/models.py
+++ api/models.py
@@ -1,8 +1,12 @@
 from config import db 
-from sqlalchemy.sql import func
+# from sqlalchemy.sql import func
+from datetime import datetime as d, timedelta as t
 
 class UrlShort(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     url_original = db.Column(db.String(250))
     url_shortened = db.Column(db.String(25))
-    date = db.Column(db.DateTime(timezone=True),default=func.now())
+    date = db.Column(
+        db.DateTime(timezone=True),
+        default=d.now() + t(30)
+    )
